42ffa44069ffa1c3b971099d0af638271d180323
Bryan <bolianchen@gmail.com>
2021-12-22 13:16:13 +0800

    Finish lab06

---
+++ lab/lab06/lab06.py
@@ -0,0 +1,93 @@
+this_file = __file__
+
+
+def make_adder_inc(a):
+    """
+    >>> adder1 = make_adder_inc(5)
+    >>> adder2 = make_adder_inc(6)
+    >>> adder1(2)
+    7
+    >>> adder1(2) # 5 + 2 + 1
+    8
+    >>> adder1(10) # 5 + 10 + 2
+    17
+    >>> [adder1(x) for x in [1, 2, 3]]
+    [9, 11, 13]
+    >>> adder2(5)
+    11
+    """
+    "*** YOUR CODE HERE ***"
+    count = 0
+    def adder(amount):
+        nonlocal count
+        result = a + amount + count
+        count += 1
+        return result
+    return adder
+
+
+def make_fib():
+    """Returns a function that returns the next Fibonacci number
+    every time it is called.
+
+    >>> fib = make_fib()
+    >>> fib()
+    0
+    >>> fib()
+    1
+    >>> fib()
+    1
+    >>> fib()
+    2
+    >>> fib()
+    3
+    >>> fib2 = make_fib()
+    >>> fib() + sum([fib2() for _ in range(5)])
+    12
+    >>> from construct_check import check
+    >>> # Do not use lists in your implementation
+    >>> check(this_file, 'make_fib', ['List'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    idx = 0
+    f0, f1 = 0, 1
+    def fib():
+        nonlocal idx, f0, f1
+        if idx == 0:
+            result = f0
+        elif idx == 1:
+            result = f1
+        else:
+            result = f0 + f1
+            temp = f0
+            f0, f1 = f1, temp + f1
+        idx += 1
+        return result
+    return fib
+
+def insert_items(lst, entry, elem):
+    """
+    >>> test_lst = [1, 5, 8, 5, 2, 3]
+    >>> new_lst = insert_items(test_lst, 5, 7)
+    >>> new_lst
+    [1, 5, 7, 8, 5, 7, 2, 3]
+    >>> large_lst = [1, 4, 8]
+    >>> large_lst2 = insert_items(large_lst, 4, 4)
+    >>> large_lst2
+    [1, 4, 4, 8]
+    >>> large_lst3 = insert_items(large_lst2, 4, 6)
+    >>> large_lst3
+    [1, 4, 6, 4, 6, 8]
+    >>> large_lst3 is large_lst
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    prev_match = False
+    for i, e in enumerate(lst):
+        if e == entry and not (entry == elem and prev_match):
+            lst.insert(i+1, elem)
+            prev_match = True
+        else:
+            prev_match = False
+    return lst
